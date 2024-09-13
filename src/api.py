d001bcf7b65a35447b1975604435d077f48dd8fb
Paulo Neto <114318023+pauloneto342@users.noreply.github.com>
2024-09-13 15:29:37 -0300

    Refactoring

--- src/api.py
+++ src/api.py
@@ -3,9 +3,9 @@ from fastapi.responses import PlainTextResponse
 from match import Match
 from player import Player
 
-matches = []
+partidas = []
 id = 0
-presentation_string = '''Sejam bem vindo ao jogo WAR!
+string_de_apresentacao = '''Sejam bem vindo ao jogo WAR!
 
 Salas disponíveis:
 
@@ -15,53 +15,52 @@ app = FastAPI()
 
 @app.get("/", response_class=PlainTextResponse)
 async def inicio():
-    global matches
-    current_match = ''''''
-    for match in matches:
-        print(match.get_id())
-        current_match += "id: " + str(match.get_id()) + " - " + match.get_name() + f" players: {match.get_number_of_players()}" + "\n"
-    return presentation_string + current_match
+    global partidas
+    partidas_disponiveis = ''''''
+    for match in partidas:
+        partidas_disponiveis += "id: " + str(match.get_id()) + " - " + match.get_name() + f" players: {match.get_number_of_players()}" + "\n"
+    return string_de_apresentacao + partidas_disponiveis
 
 @app.post("/criar-partida")
-async def criar_partida():
-    global id, presentation_string, matches
-    new_match = Match(match_name=f"Pipoca do maior {id}", match_id=id)
-    matches.append(new_match)
+async def criar_partida(numero_de_jogadores_para_iniciar : int):
+    global id, string_de_apresentacao, partidas
+    nova_partida = Match(match_name=f"Partida {id}", match_id=id, number_of_players_to_start=numero_de_jogadores_para_iniciar)
+    partidas.append(nova_partida)
+    string_de_status = f"partida criada com sucesso! id da partida: {id}"
     id += 1
-    return f"partida criada com sucesso! id da partida: {id}"
+    return string_de_status
 
 @app.post("/entrar-na-partida")
 async def entrar_na_partida(nome_usuario:str , id_da_partida:int):
-    global matches
-    user = Player(nome_usuario)
-    for match in matches:
-        if match.get_id() == id_da_partida:
-            match.join_match(user)
-            return f"Você entrou na partida {match.get_id()}"
-    
+    global partidas
+    usuario = Player(nome_usuario)
+    for partida in partidas:
+        if partida.get_id() == id_da_partida:
+            partida.join_match(usuario)
+            return f"Você entrou na partida {partida.get_id()}"
     return "Não foi possível entrar na partida"
 
-@app.get("/{id}/{player_name}", response_class=PlainTextResponse)
-def tela_jogo(id:int, player_name:str):
-    global matches
-    if (matches[id].check_if_player_exists(player_name)):
-        player = matches[id].get_player(player_name)
-        objetivo = player.get_objective()
-        cor_do_exercito = player.get_color()
+@app.get("/{id_da_partida}/{nome_do_jogador}", response_class=PlainTextResponse)
+async def tela_jogo(id_da_partida:int, nome_do_jogador:str):
+    global partidas
+    if (partidas[id_da_partida].check_if_player_exists(nome_do_jogador)):
+        jogador = partidas[id_da_partida].get_player(nome_do_jogador)
+        objetivo = jogador.get_objective()
+        cor_do_exercito = jogador.get_color()
         mensagem_objetivo = f"Seu objetivo: {objetivo.get_objective()}\n"
-        string_de_exibicao = f"cor do exército: {cor_do_exercito}\n" + mensagem_objetivo + matches[id].show_game_status()
+        string_de_exibicao = f"cor do exército: {cor_do_exercito}\n" + mensagem_objetivo + partidas[id_da_partida].return_game_status()
         return string_de_exibicao
     else:
         return "Você não tem acesso a essa partida"
 
-@app.post("/{id}/{player_name}/set-color")
-def selecionar_cor(id:int, player_name: str, cor:str):
-    cor_disponiveis = matches[id].avaliable_colors()
+@app.post("/{id_da_partida}/{nome_do_jogador}/set-color")
+async def selecionar_cor(id_da_partida:int, nome_do_jogador: str, cor:str):
+    cor_disponiveis = partidas[id_da_partida].avaliable_colors()
     if (cor in cor_disponiveis):
-        player = matches[id].get_player(player_name)
-        player.set_color(cor)
-        matches[id].remove_color(cor)
-        return f"A cor {cor} foi aplicada ao player {player.get_name()}"
+        jogador = partidas[id_da_partida].get_player(nome_do_jogador)
+        jogador.set_color(cor)
+        partidas[id_da_partida].remove_color(cor)
+        return f"A cor {cor} foi aplicada ao player {jogador.get_name()}"
     else:
         return 'Cor inválida ou em uso por outro player'
 
--- src/match.py
+++ src/match.py
@@ -1,20 +1,18 @@
 from map import Map
-from objective import Objective
-from territory import Territory
 from player import Player
-import io
+from datamanager import DataManager
 import random
 
 class Match():
     
-    def __init__(self, match_name, match_id):
+    def __init__(self, match_name, match_id, number_of_players_to_start):
         self.__match_name = match_name
         self.__match_id = match_id
+        self.__amount_to_start = number_of_players_to_start
         self.__match_started = False
         self.__all_colors_checked = False
         self.__colors = ['verde', 'azul', 'amarelo', 'preto', 'branco', 'vermelho']
-        self.tempo_de_espera = 0
-        self.__vez_de_jogar = Player("null")
+        self.__current_player = Player("null")
         self.__map = Map()
         self.__objectives = []
         self.__players = []
@@ -27,25 +25,12 @@ class Match():
         return self.__match_name
     
     def __load_data(self):
-        with io.open("data/objectives.txt", mode='r', encoding='utf-8') as objective_file:
-           for objective in objective_file:
-               normalized_string = objective.replace('\n', '')
-               self.__objectives.append(Objective(text=normalized_string))
-        
-        with io.open("data/territories.txt", mode='r', encoding='utf-8') as territory_file:
-           for territory in territory_file:
-               normalized_territory= territory.replace('\n', '')
-               payload_territories = normalized_territory.split(",")
-               self.__map.add_territory(Territory(name=payload_territories[0], region=payload_territories[1]))
+        DataManager.load_data_to(self.__objectives, self.__map)
     
-    def join_match(self, player):
-        self.__players.append(player)
-        random_objective_pos = random.randint(0, len(self.__objectives) - 1)
-        player.set_objective(self.__objectives[random_objective_pos])
-
-        if (self.get_number_of_players() >= 2 and self.__match_started == False):
+    def __check_if_game_should_start(self):
+        if (self.get_number_of_players() >= self.__amount_to_start and self.__match_started == False):
             self.__match_started = True
-            self.__vez_de_jogar = self.__players[0]
+            self.__current_player = self.__players[0]
 
             player_id = 0
             territories = self.__map.get_territories()
@@ -56,16 +41,20 @@ class Match():
                     random_territory_pos = random.randint(0, len(territories) - 1)
 
                 num_sorteados.append(random_territory_pos)
-                print(f"Numero sorteado: {random_territory_pos}")
                 territories[random_territory_pos].set_dominant_player(self.__players[player_id])
                 player_id += 1
                 if (player_id >= len(self.__players)):
                     player_id = 0
                 territories[random_territory_pos].add_army(1)
             
-
             self.__map.replace_territories(territories)
 
+    def join_match(self, player):
+        self.__players.append(player)
+        random_objective_pos = random.randint(0, len(self.__objectives) - 1)
+        player.set_objective(self.__objectives[random_objective_pos])
+        self.__objectives.pop(random_objective_pos)
+        self.__check_if_game_should_start()
 
     def avaliable_colors(self):
         return self.__colors
@@ -80,10 +69,10 @@ class Match():
             if player.get_color() == None:
                 self.__all_colors_checked = False
         
-    def show_game_status(self):
+    def return_game_status(self):
         self.__check_color_selection()
         if (self.__match_started and self.__all_colors_checked):
-            string_player_da_rodada = f'Vez de jogar: {self.__vez_de_jogar.get_name()}\n\n'
+            string_player_da_rodada = f'Vez de jogar: {self.__current_player.get_name()}\n\n'
             string_territories= ''''''
             for territory in self.__map.get_territories():
                 string_territories += "Região: " + territory.get_region() + '\t\t' 'Território: ' + territory.get_name() + '\t\t' + 'Tropas: ' + str(territory.get_army()) + '\t\t' + 'player_dominante: ' + territory.get_dominant_player_name() + '\n'
